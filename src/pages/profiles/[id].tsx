c536afc20509f264371dfb3afa9a8cf58d980cd7
Rynhardt Smith <rynhardt.smith@fanplayr.com>
2023-05-20 10:47:54 +1000

    update changes

--- src/pages/profiles/[id].tsx
+++ src/pages/profiles/[id].tsx
@@ -126,15 +126,27 @@ export async function getStaticProps(
   context: GetStaticPropsContext<{ id: string }>
 ) {
   const id = context.params?.id;
-  if (id == null) return { redirect: { destination: "/" } };
+
+  if (id == null) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const ssg = ssgHelper();
-
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
   await ssg.profile.getById.prefetch({ id });
 
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-  return { props: { trpcState: ssg.dehydrate(), id } };
+  return {
+    props: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
 }
 
 export default ProfilePage;
